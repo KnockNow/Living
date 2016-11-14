aa4f84bc302375522ac3abbe95ee99a409cf4bc9
AlexBDev <bussieres.alexis.dev@gmail.com>
2016-11-14 16:37:50 +0100

    Add feature research

--- js/script.js
+++ js/script.js
@@ -10,5 +10,31 @@
         player.attr('src', songSrc);
         currentSong.text(songName);
       });
+
+      // Search feature
+      var songList = $('#songs li');
+      var songListName = [];
+
+      $.each(songList, function(index) {
+        songListName.push($(this).text());
+      });
+
+      $('input.search').on('keyup', function() {
+          var value = $.trim($(this).val());
+
+          if (value.length === 0) {
+              $(songList).show();
+
+              return false;
+          }
+
+          var regex = new RegExp(value, "i");
+
+          $.each(songListName, function (index) {
+              if (!regex.test(songListName[index])) {
+                $(songList[index]).hide();
+              }
+          });
+      });
   });
 })(jQuery);
