c24969ec6f02da7916660ea03e387c71374fd39a
Will Helie <william.heliejoly@gmail.com>
2016-11-15 01:30:48 +0100

    Player DIY

--- js/designJs/script.js
+++ js/designJs/script.js
@@ -1,4 +1,8 @@
 $( document ).ready(function() {
+    // View display : Only 'home', 'music', 'disc'
+    var view = 'home';
+    //Menu show : Only bool
+    var smallMenu = false;
 
   if ($(window).width() < 1235) {
     $('.smallMenu').css('display', 'block');
@@ -34,4 +38,129 @@ $( document ).ready(function() {
       }
 
     });
+
+    // Display Small Menu
+    $('.smallMenu').click(function() {
+        if(smallMenu) {
+            // Hide menu
+            $('.control-mobile').slideUp();
+            $('#iconSmallMenu').removeClass('ion-close').addClass('ion-navicon-round');
+            smallMenu = false;
+        }else{
+            // Show menu
+            $('.control-mobile').slideDown();
+            $('#iconSmallMenu').removeClass('ion-navicon-round').addClass('ion-close');
+            smallMenu = true;
+        }
+    })
+
+    // Change view
+    $('#home').click(function() {
+        //Change menu
+        $('#music').removeClass('controlActive');
+        $('#disc').removeClass('controlActive');
+        $('#home').addClass('controlActive');
+        //m
+        $('#musicSmall').removeClass('controlActive');
+        $('#discSmall').removeClass('controlActive');
+        $('#homeSmall').addClass('controlActive');
+
+        //View
+        if (view != 'home') {
+            $('.myMusic').hide();
+            $('.discover').hide();
+            $('.main').fadeIn();
+            view = 'home';
+        }
+    })
+
+    $('#music').click(function(){
+        //Change menu
+        $('#home').removeClass('controlActive');
+        $('#disc').removeClass('controlActive');
+        $('#music').addClass('controlActive');
+        //m
+        $('#homeSmall').removeClass('controlActive');
+        $('#discSmall').removeClass('controlActive');
+        $('#musicSmall').addClass('controlActive');
+
+        //View
+        if (view != 'music') {
+            $('.discover').hide();
+            $('.main').hide();
+            $('.myMusic').fadeIn();
+            view = 'music';
+        }
+    })
+
+    $('#disc').click(function(){
+        //Change menu
+        $('#music').removeClass('controlActive');
+        $('#home').removeClass('controlActive');
+        $('#disc').addClass('controlActive');
+        //m
+        $('#musicSmall').removeClass('controlActive');
+        $('#homeSmall').removeClass('controlActive');
+        $('#discSmall').addClass('controlActive');
+
+        //View
+        if (view != 'disc') {
+            $('.main').hide();
+            $('.myMusic').hide();
+            $('.discover').fadeIn();
+            view = 'disc';
+        }
+    })
+
+    // For mobile
+    $('#homeSmall').click(function() {
+        $('#musicSmall').removeClass('controlActive');
+        $('#discSmall').removeClass('controlActive');
+        $('#homeSmall').addClass('controlActive');
+        //c
+        $('#music').removeClass('controlActive');
+        $('#disc').removeClass('controlActive');
+        $('#home').addClass('controlActive');
+        //View
+        if (view != 'home') {
+            $('.myMusic').hide();
+            $('.discover').hide();
+            $('.main').show();
+            view = 'home';
+        }
+    })
+
+    $('#musicSmall').click(function(){
+        $('#homeSmall').removeClass('controlActive');
+        $('#discSmall').removeClass('controlActive');
+        $('#musicSmall').addClass('controlActive');
+        //c
+        $('#home').removeClass('controlActive');
+        $('#disc').removeClass('controlActive');
+        $('#music').addClass('controlActive');
+        //View
+        if (view != 'music') {
+            $('.discover').hide();
+            $('.main').hide();
+            $('.myMusic').show();
+            view = 'music';
+        }
+    })
+
+    $('#discSmall').click(function(){
+        $('#musicSmall').removeClass('controlActive');
+        $('#homeSmall').removeClass('controlActive');
+        $('#discSmall').addClass('controlActive');
+        //c
+        $('#music').removeClass('controlActive');
+        $('#home').removeClass('controlActive');
+        $('#disc').addClass('controlActive');
+        //View
+        if (view != 'disc') {
+            $('.main').hide();
+            $('.myMusic').hide();
+            $('.discover').show();
+            view = 'disc';
+        }
+    })
 });
--- js/script.js
+++ js/script.js
@@ -1,14 +1,74 @@
 (function($){
   $(function(){
+      var v = document.getElementsByTagName("audio")[0];
+      var bar = document.getElementById('bar');
+      var cursor = document.getElementById('cursor');
       var player = $('audio');
       var currentSong = $('#currentSong');
 
-      $('#songs').on('click', 'li', function() {
-        var songSrc = $(this).data('source');
-        var songName = $(this).text();
+      var idListen = '';
+      var idNext = '';
 
-        player.attr('src', songSrc);
-        currentSong.text(songName);
+      $('#songs').on('click', 'li', function() {
+            var songSrc = $(this).data('source');
+            var songName = $(this).text();
+
+            //Take the select id
+            idNext = $(this).closest('li').attr('id');
+
+            //Style li played
+            if (idListen) {
+                $('#'+idListen).removeClass('musicPlayed');
+                $('#'+idListen).children('span').removeClass('ion-pause').addClass('ion-play');
+            }
+
+            if (idListen == idNext) {
+                if (v.paused) {
+                    $(this).children('span').removeClass('ion-play').addClass('ion-pause');
+                    v.play();
+                }else{
+                    $(this).children('span').removeClass('ion-pause').addClass('ion-play');
+                    $(this).addClass('musicPlayed');
+                    v.pause();
+
+                }
+                return false;
+            }
+
+            $(this).addClass('musicPlayed');
+            $(this).children('span').removeClass('ion-play').addClass('ion-pause');
+            idListen = idNext;
+
+            player.attr('src', songSrc);
+            currentSong.text(songName);
+
+            v.play();
+
+            where = function(elem){
+                return elem.currentTime;
+            };
+
+            duree = function(elem){
+                return elem.duration;
+            };
+
+            dure = function(elem) {
+                return elem.duration;
+            }
+
+            setInterval(function(){
+              var seconde, minute, heure, duree, dureeTotale, pourcentage;
+              duree = where(v);
+              dureeTotale = dure(v);
+              pourcentage = duree / dureeTotale;
+              bar.style.width = (100 - (100 - (pourcentage * 100))) + "%";
+              cursor.style.left = (100 - (100 - (pourcentage * 100))) + "%";
+              duree = Math.round(duree);
+              seconde =  duree % 60;
+              minute = (duree - seconde) / 60;
+              heure = (duree - seconde - (minute * 60)) / 3600;
+          }, 200);
+
       });
 
       // Search feature
